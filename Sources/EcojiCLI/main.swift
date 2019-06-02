96b26d0eab406bd85bc5b6793ffba3c5593abc81
Robin Diddams <robindiddams@gmail.com>
2019-06-01 20:03:30 -0500

    removed CLI🗑

--- Sources/EcojiCLI/main.swift
+++
@@ -1,9 +0,0 @@
-import Ecoji
-import Foundation
-
-if let input = readLine() {
-    let encoder = EcojiEncoder()
-    if let encoded = encoder.encode(from: input) {
-        print(encoded)
-    }
-}
